b932330a0a4895533a25279675345569ddf30990
iMaary <m.rodriguesnave@gmail.com>
2019-08-03 13:27:18 -0300

    primeiro commit

---
+++ Assets/Script/Dialogo.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogo : MonoBehaviour
+{
+    public Respostas resposta;
+    [SerializeField] private Text textoResposta;
+    private string padrao = "";
+    private string[] resp1;
+
+    void Start()
+    {
+        resp1 = new string[] { "você apertou na primeira perguntar", "você apertou na segunda perguntar" };
+    }
+
+    void Update()
+    {
+        ControllerTexts();
+    }
+
+    private void ControllerTexts()
+    {
+        switch (resposta)
+        {
+            case Respostas.respostaUm:
+                StartCoroutine(ApareceTexto(resp1[0]));
+                PadraoTextos();
+                break;
+            case Respostas.respostaDois:
+                StartCoroutine(ApareceTexto(resp1[1]));
+                PadraoTextos();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void PadraoTextos()
+    {
+        resposta = Respostas.nulo;
+    }
+
+    public IEnumerator ApareceTexto(string resp)
+    {
+        for (int i = 0; i < resp.Length; i++)
+        {
+            padrao = resp.Substring(0, i);
+            textoResposta.text = padrao;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+}
---
+++ Assets/Script/Perguntas.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Button))]
+public class Perguntas : MonoBehaviour
+{
+    [SerializeField] private Dialogo dl;
+    [SerializeField] private Respostas rp;
+    private Button b;
+
+    void Start()
+    {
+        SetDefault();
+    }
+
+    void FixedUpdate()
+    {
+    }
+
+    private void SetDefault()
+    {
+        GetComponent<Button>().onClick.AddListener(Click);
+    }
+
+    public void Click()
+    {
+        dl.resposta = rp;
+    }
+}
